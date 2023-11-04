3552518750a8b9013872048ff8f9c06b450a55af
Liam Schultz <lschult7@uwo.ca>
2023-11-04 11:33:46 -0400

    Improved jumping and removed CMakeList

--- Player.cpp
+++ Player.cpp
@@ -15,6 +15,7 @@ Player::Player(sf::Vector2<unsigned int> windowSize) {
 }
 
 void Player::Update(int timeElapsed) {
+    timeElapsed *= simSpeed;
     //left and right motion
     if(sf::Keyboard::isKeyPressed(sf::Keyboard::A) && shape->getPosition().x-speed*timeElapsed > lowerXBound) {
         shape->move(-speed*timeElapsed, 0);
@@ -25,23 +26,24 @@ void Player::Update(int timeElapsed) {
     //
 
     //initial jump
-    if(!jumped && ay < initialJumpAcceleration && ((sf::Keyboard::isKeyPressed(sf::Keyboard::W) || sf::Keyboard::isKeyPressed(sf::Keyboard::Space)))) {
-        ay += initialJumpAcceleration;
+    if(!jumped && ((sf::Keyboard::isKeyPressed(sf::Keyboard::W) || sf::Keyboard::isKeyPressed(sf::Keyboard::Space)))) {
+        vy = initialJumpVelocity;
         jumped = true;
     }
     //midair control (jump higher/delay landing)
     else if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) || sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
-        ay += midJumpAcceleration;
+        ay = midJumpAcceleration+gravity;
     }
+    else ay = gravity;
 
     //change velocity based on acceleration up to some terminal velocity
-    if (vy + ay >= terminalVelocity) vy+=ay;
+    if (vy + ay*timeElapsed >= terminalVelocity) vy+=ay*timeElapsed;
     else vy = terminalVelocity;
     //
 
     //change acceleration until matching gravity
-    if (ay > gravity && ay + gravity > gravity) ay += gravity;
-    else if (ay + gravity <= gravity) ay = gravity;
+    //if (ay > gravity && ay + gravity > gravity) ay += gravity;
+    //else if (ay + gravity <= gravity) ay = gravity;
     //
 
     //change position until you hit the floor
--- Player.h
+++ Player.h
@@ -14,14 +14,15 @@ public:
     sf::Shape* getShape();
 private:
     sf::RectangleShape* shape;
-    const float speed = 0.0005;
+    const float speed = 0.002;
     const unsigned int lowerXBound = 0; //temporary
     unsigned int upperXBound; //temporary
     unsigned int lowerYBound; //temporary
-    const float gravity = -0.000002;
-    const float initialJumpAcceleration = 0.00003;
-    const float midJumpAcceleration = 0.0000005;
-    const float terminalVelocity = -0.001;
+    const float gravity = -0.0000002;
+    const float initialJumpVelocity = 0.008;
+    const float midJumpAcceleration = 0.0000001;
+    const float terminalVelocity = -0.1;
+    const float simSpeed = 0.25;
     float vy = 0;
     float ay = 0;
     bool jumped = false;
