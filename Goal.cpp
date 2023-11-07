ea3f255806db8bbeb42c32d5aeb14f92defd68d0
kbotchwa <kbotchwa@uwo.ca>
2023-11-07 18:19:00 -0500

    Goal Deoxygen comment

--- Goal.cpp
+++ Goal.cpp
@@ -7,6 +7,11 @@
 
 #include "Goal.h"
 
+/**
+ * @brief sets the colour and position of end point
+ * @param size
+ * @param pos
+ */
 Goal::Goal(sf::Vector2f size, sf::Vector2f pos) : position(pos), Object(size) {
     shape->setPosition(position.x, position.y);
     shape->setFillColor(sf::Color::Green);
--- Goal.h
+++ Goal.h
@@ -13,9 +13,17 @@
 
 class Goal : public Object {
 public:
+    /**
+     * @brief Goal constructor
+     * @param size
+     * @param pos
+     */
     Goal(sf::Vector2f size, sf::Vector2f pos);
 
 private:
+    /**
+     * @brief postion variable
+     */
     sf::Vector2f position;
 };
 
